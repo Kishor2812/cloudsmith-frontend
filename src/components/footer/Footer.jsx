b284a6a1fe55f5e089da53a956482b851ac48644
Kishor2812 <kishorravichandran28@gmail.com>
2026-08-08 07:35:31 +0530

    Initial Frontend

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import {
-  FaFacebookF,
+  // FaFacebookF,
   FaLinkedinIn,
-  FaGithub,
+  // FaGithub,
   FaInstagram,
   FaEnvelope,
   FaPhoneAlt,
